a9e0f9e5519408fd650f896bc297a785255dc702
ACCOUNT MAIL SERVICES <jasimarifali@gmail.com>
2021-10-26 10:39:30 +0500

    my first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const express = require('express')
+const path = require('path')
+const methodOverride = require('method-override')
+let publicDirectoryPath = path.join(__dirname, './demo7/public')
+
+const app = express()
+app.set('view engine', 'ejs')
+//app.set('views', path.join(__dirname,'./demo7/views'))
+app.use('/demo7',express.static(path.join(__dirname, './demo7/public')))
+app.use('/demo1',express.static(path.join(__dirname, './demo1/public')))
+
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
+// app.use(express.static(publicDirectoryPath)) 
+
+
+app.get('/demo7', (req,res)=>{
+    app.set('views', path.join(__dirname,'./demo7/views'))
+
+    res.render('index')
+}) 
+app.get('/demo1', (req,res)=>{
+    app.set('views', path.join(__dirname,'./demo1/views'))
+    res.render('index')
+}) 
+const port =  process.env.PORT || 8000
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+}) 
+
