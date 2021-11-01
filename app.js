adb8b6f8dcfcae0906c9ad15d4d5c02d6079d986
ACCOUNT MAIL SERVICES <jasimarifali@gmail.com>
2021-11-01 08:52:02 +0500

    routes added

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const path = require('path')
 const methodOverride = require('method-override')
+const data = require('./data.js')
+
+// console.log(data)
 let publicDirectoryPath = path.join(__dirname, './demo7/public')
 
 const app = express()
+app.use(express.urlencoded({extended:true}))
+
 app.set('view engine', 'ejs')
-//app.set('views', path.join(__dirname,'./demo7/views'))
-app.use('/demo7',express.static(path.join(__dirname, './demo7/public')))
+app.set('views', path.join(__dirname,'./demo7/views'))
+app.use('/',express.static(path.join(__dirname, './demo7/public')))
 app.use('/demo1',express.static(path.join(__dirname, './demo1/public')))
 
 app.use(express.urlencoded({extended:true}))
@@ -19,22 +24,64 @@ app.use(methodOverride('_method'))
 
 
 
-app.get('/demo7', (req,res)=>{
-    app.set('views', path.join(__dirname,'./demo7/views'))
+app.get('/', (req,res)=>{
+   // app.set('views', path.join(__dirname,'./demo7/views'))
     let route = "partials/_content"
     res.render('index',{route})
 })
 
-app.get('/demo7/create-sales-order', (req,res)=>{
-    app.set('views', path.join(__dirname,'./demo7/views'))
+app.get('/create-sales-order', (req,res)=>{
+  //  app.set('views', path.join(__dirname,'./demo7/views'))
     let route = "pages/salesOrderForm"
     res.render('index', {route})
 })
-app.get('/demo7/sales-orders', (req,res)=>{
-    app.set('views', path.join(__dirname,'./demo7/views'))
+app.get('/sales-orders', (req,res)=>{
+   // app.set('views', path.join(__dirname,'./demo7/views'))
     let route = "pages/table"
+    res.render('index', {route,data}) 
+})
+app.get('/view',async (req,res)=>{
+   // app.set('views', path.join(__dirname,'./demo7/views'))
+
+    let route = "pages/transaction"
     res.render('index', {route})
 })
+ 
+app.get('/sales-orders/:id', async (req,res)=>{
+    app.use('/sales-orders',express.static(path.join(__dirname, './demo7/public')))
+    var {id} = req.params
+    let route = "pages/transaction"
+    var salesOrderData = data.filter((el)=>{
+        if(el._id == id){
+            return el
+        }
+    })
+    console.log("salesorder data", salesOrderData)
+    res.render('index', {route,salesOrderData})
+})
+
+app.get('/sales-orders/:id/edit', async (req,res)=>{
+    var {id} = req.params
+    app.use('/sales-orders/'+id ,express.static(path.join(__dirname, './demo7/public')))
+
+    let route = "pages/transaction"
+    res.render('index', {route})
+})
+// app.put('/sales-orders/:id', async (req,res)=>{
+//     var {id} = req.params
+//     let route = "pages/salesOrderForm"
+//     res.redirect(`/sales-orders/${campground._id}`)
+// })
+
+// app.delete('/sales-orders/:id',async(req,res)=>{
+//     var {id} = req.params
+//     res.redirect('/view')
+// })
+
+
+
+
+
 app.get('/demo1', (req,res)=>{
     app.set('views', path.join(__dirname,'./demo1/views'))
     res.render('index')
---
+++ data.js
@@ -0,0 +1,192 @@
+module.exports = [
+    {
+      "_id": "1",
+      "tranid": "SO112",
+      "trandate": "9/23/2021",
+      "quantity": "4",
+      "amount": "4884.75",
+      "statusref": [
+        {
+          "value": "PurchOrd:A",
+          "text": "Pending Supervisor Approval"
+        }
+      ],
+      "internalid": [
+        {
+          "value": "22164",
+          "text": "22164"
+        }
+      ],
+      "location": [
+        {
+          "value": "7",
+          "text": "Singapore : East Coast"
+        }
+      ],
+      "approvalstatus": [
+        {
+          "value": "1",
+          "text": "Pending Approval"
+        }
+      ],
+      "items": [
+        {
+          "id": "1",
+          "name": "shing shing wooden door",
+          "rate": 2
+        }
+      ]
+    },
+    {
+      "_id": "2",
+      "tranid": "SO11244",
+      "trandate": "9/27/2021",
+      "quantity": "1",
+      "amount": "4884.75",
+      "statusref": [
+        {
+          "value": "PurchOrd:A",
+          "text": "Pending Supervisor Approval"
+        }
+      ],
+      "internalid": [
+        {
+          "value": "22164",
+          "text": "22164"
+        }
+      ],
+      "location": [
+        {
+          "value": "7", 
+          "text": "Singapore : East Coast"
+        }
+      ],
+      "approvalstatus": [
+        {
+          "value": "1",
+          "text": "Pending Approval"
+        }
+      ],
+      "items": [
+        {
+          "id": "1",
+          "name": "shing shing wooden door",
+          "rate": 2
+        }
+      ]
+    },
+    {
+      "_id": "3",
+      "tranid": "SO11243",
+      "trandate": "9/28/2021",
+      "quantity": "8",
+      "amount": "4884.75",
+      "statusref": [
+        {
+          "value": "PurchOrd:A",
+          "text": "Pending Supervisor Approval"
+        }
+      ],
+      "internalid": [
+        {
+          "value": "22164",
+          "text": "22164"
+        }
+      ],
+      "location": [
+        {
+          "value": "7",
+          "text": "Singapore : East Coast"
+        }
+      ],
+      "approvalstatus": [
+        {
+          "value": "1",
+          "text": "Pending Approval"
+        }
+      ],
+      "items": [
+        {
+          "id": "1",
+          "name": "shing shing wooden door",
+          "rate": 2
+        }
+      ]
+    },
+    {
+      "_id": "4",
+      "tranid": "SO11243",
+      "trandate": "9/26/2021",
+      "quantity": "3",
+      "amount": "4884.75",
+      "statusref": [
+        {
+          "value": "PurchOrd:A",
+          "text": "Pending Supervisor Approval"
+        }
+      ],
+      "internalid": [
+        {
+          "value": "22164",
+          "text": "22164"
+        }
+      ],
+      "location": [
+        {
+          "value": "7",
+          "text": "Singapore : East Coast"
+        }
+      ],
+      "approvalstatus": [
+        {
+          "value": "1",
+          "text": "Pending Approval"
+        }
+      ],
+      "items": [
+        {
+          "id": "1",
+          "name": "shing shing wooden door",
+          "rate": 2
+        }
+      ]
+    },
+    {
+      "_id": "5",
+      "tranid": "SO116743",
+      "trandate": "9/24/2021",
+      "quantity": "3",
+      "amount": "4884.75",
+      "statusref": [
+        {
+          "value": "PurchOrd:A",
+          "text": "Pending Supervisor Approval"
+        }
+      ],
+      "internalid": [
+        {
+          "value": "22164",
+          "text": "22164"
+        }
+      ],
+      "location": [
+        {
+          "value": "7",
+          "text": "Singapore : East Coast"
+        }
+      ],
+      "approvalstatus": [
+        {
+          "value": "1",
+          "text": "Pending Approval"
+        }
+      ],
+      "items": [
+        {
+          "id": "1",
+          "name": "shing shing wooden door",
+          "rate": 2
+        }
+      ]
+    }
+  ]
